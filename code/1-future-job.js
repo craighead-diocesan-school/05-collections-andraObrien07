5080330208fc5d8b7f4638dbc08716d689231cfd
andraObrien07 <comanan09@craighead.school.nz>
2023-06-24 11:38:21 +1200

    this is hard

--- code/1-future-job.js
+++ code/1-future-job.js
@@ -3,14 +3,16 @@
 // ##################################
 
 let jobs = [
-  'astronaut',
-  'firefighter',
-  'police officer',
-  'space doctor',
+  ' astronaut ',
+  ' firefighter ',
+  ' police officer ',
+  ' space doctor ',
+  ' digi tech teacher',
+  'sucba diver',
 ]
 
 function getJob() {
   let index = 0
 
-  alert('Your future job will be' + jobs[0])
+  alert('Your future job will be a' + jobs[3])
 }
